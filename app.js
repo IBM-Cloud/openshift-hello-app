7b0084e1677def22fd03d7808ca666859cffeb11
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-10-08 11:20:53 +0200

    set default to 8080

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || process.env.HELLO_NODE_APP_SERVICE_PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
