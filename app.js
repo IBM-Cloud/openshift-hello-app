c99a92518cf7747f80666f78e21ba50f49d432db
Frederic Lavigne <frederic.lavigne@ibm.com>
2019-10-24 10:34:11 -0500

    add environment to apps

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || process.env.HELLO_NODE_APP_SERVICE_PORT || 8080;
+const environment = process.env.ENVIRONMENT || 'unknown';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello World!\n');
+  res.end(`Hello World! I'm running in the project named "${environment}"\n`);
 });
 
 server.listen(port, () => {
