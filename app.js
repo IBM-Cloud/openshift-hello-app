b0b081e73205f34f45b2c7b222b7d980b073bbd6
Frederic Lavigne <frederic.lavigne@ibm.com>
2019-10-24 10:05:34 -0500

    show openshift namespace

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || process.env.HELLO_NODE_APP_SERVICE_PORT || 8080;
+const namespace = process.env.OPENSHIFT_BUILD_NAMESPACE || 'unknown';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello World!\n');
+  res.end(`Hello World! I'm running in the project named "${namespace}"\n`);
 });
 
 server.listen(port, () => {
