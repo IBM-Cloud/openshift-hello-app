39bb81c41ec38022f05facee27e08f0aba3088bd
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-10-25 19:13:39 +0530

    Updated the app

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http = require('http');
+      fs   = require('fs');
 const port = process.env.PORT || process.env.HELLO_NODE_APP_SERVICE_PORT || 8080;
 const environment = process.env.ENVIRONMENT || 'unknown';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end(`Hello World! I'm running in the project named "${environment}"\n`);
-});
-
-server.listen(port, () => {
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }
+    html=html+"<p>from</p> <h3>"+environment+" project</h3>"; 
+    const server=http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);
+        response.end();  
+    }).listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
+});
 });
